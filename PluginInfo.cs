03dbaaf36c7a10f79caad1a15424b91e201b78e5
TylosinGT <141987315+TylosinGT@users.noreply.github.com>
2023-08-10 23:23:14 -0700

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace StumpMirrorFix
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.tylosingt.gorillatag.stumpmirrorfix";
+        public const string Name = "StumpMirrorFix";
+        public const string Version = "1.0.0";
+    }
+}
